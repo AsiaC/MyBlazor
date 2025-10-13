fd26c6f399c2bbd058193a7e7d461e218343a52d
czapl <czapl@THINKBOOK-JC>
2025-10-13 10:41:52 +0200

    Add project files.

---
+++ MyServerManagement/Models/Server.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+
+        [Required] //https://learn.microsoft.com/en-gb/dotnet/api/system.componentmodel.dataannotations?view=net-8.0
+        public string? Name { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+    }
+}
