2ef348a21ef22b4c626e1bae7a951f35d0cf2e5b
AsiaC <czaplicka.asia@gmail.com>
2026-02-04 14:40:34 +0100

    Policy based authorization

--- MyServerManagement/Program.cs
+++ MyServerManagement/Program.cs
@@ -29,6 +29,10 @@ builder.Services.AddAuthentication(options =>
 })
     .AddIdentityCookies();
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Manager", policy => policy.RequireClaim("Role", "General Manager"));  
+});
 var connectionString = builder.Configuration.GetConnectionString("IdentityDatabase") ?? throw new InvalidOperationException("Connection string 'IdentityDatabase' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
